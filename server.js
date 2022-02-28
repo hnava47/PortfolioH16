abdf081eadafb42e27116d948c714e9ffae8249e
Hector Nava <hector.r.nava44@gmail.com>
2022-02-28 15:23:23 -0700

    Added middleware to handle redirect to HTTPS

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const routes = require('./router');
-import sslRedirect from 'heroku-ssl-redirect';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,8 +14,14 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(sslRedirect());
 app.use(express.static(path.join(__dirname, 'public')));
+if (process.env.NODE_ENV === 'production') {
+    app.use( (req, res) => {
+        if (req.header('x-forwarded-proto') !== 'https:') {
+            res.redirect(`https://${req.header('host')}${req.url}`);
+        }
+    });
+}
 app.use(routes);
 
 app.listen(PORT, () => console.log(`Server listening to PORT: ${PORT}`));
