fae2630476cb44dcc9d77418c85f9352f3413dbb
Hector Nava <hector.r.nava44@gmail.com>
2022-02-28 16:02:43 -0700

    Moved ssl redirect to after routes

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const hbs = exphbs.create();
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(sslRedirect());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
+app.use(sslRedirect());
 
 app.listen(PORT, () => console.log(`Server listening to PORT: ${PORT}`));
